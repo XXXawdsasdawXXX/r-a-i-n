7fe9be7eaae92d00efca9ab77bc83727f50616ae
=) <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2026-06-16 13:39:07 +0300

    wip context menu

---
+++ Assets/Code/CoreGame/Entities/Select/Impact/HoveredImpactActiveObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace CoreGame.Entities.Select
+{
+    [Serializable]
+    public class HoveredImpactActiveObjects : SelectableImpact
+    {
+        [SerializeField] private GameObject[] _objects;
+        
+        
+        public override void Hovered(bool isHover)
+        {
+            foreach (GameObject gameObject in _objects)
+            {
+                gameObject.SetActive(isHover);
+            }
+        }
+
+        public override  void Pressed()
+        {
+         
+        }
+    }
+}
---
+++ Assets/Code/CoreGame/Entities/Select/Impact/SelectableImpact.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreGame.Entities.Select
+{
+    public abstract class SelectableImpact
+    {
+         public abstract void Hovered(bool isHover);
+
+         public abstract void Pressed();
+    }
+}
